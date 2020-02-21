51c631328be481e9c1a7024af746409f93d92fbf
sky11fur <sky11fur@gmail.com>
2020-02-21 15:51:00 +0900

    update crawl_wather.py

--- crawl_wather.py
+++ crawl_wather.py
@@ -1,7 +1,6 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-
 def get_forecast_today(apikey, long, lati, uni):
     try:
         url = 'https://api.darksky.net/forecast/' + apikey + '/' + long + ',' + lati + '?' + 'units=' + uni + "&lang=ja"
@@ -12,7 +11,6 @@ def get_forecast_today(apikey, long, lati, uni):
 
     except urllib.error.HTTPError as e:
         print('HTTPError: ', e)
-
     except json.JSONDecodeError as e:
         print('JSONDecodeError: ', e)
 
@@ -21,7 +19,6 @@ def get_forecast_today(apikey, long, lati, uni):
 def get_forecast_timemachine(apikey, long, lati, time, uni):
     try:
         url = 'https://api.darksky.net/forecast/' + apikey + '/' + long + ',' + lati +  ',' + time + '?' + 'units=' + uni + "&lang=ja"
-        print(url)
         res = urllib.request.urlopen(url)
         # json_loads()でPythonオブジェクトに変換
         data = json.loads(res.read().decode('utf-8'))
@@ -29,16 +26,10 @@ def get_forecast_timemachine(apikey, long, lati, time, uni):
 
     except urllib.error.HTTPError as e:
         print('HTTPError: ', e)
-
     except json.JSONDecodeError as e:
         print('JSONDecodeError: ', e)
 
     return data
-    
-
-
-
-
 
 if __name__ == "__main__":
 
@@ -57,6 +48,7 @@ if __name__ == "__main__":
     parser.add_argument('--long', type=str, default='37.8267')
     parser.add_argument('--lati', type=str, default='-122.4233')
     parser.add_argument('--uni', type=str, default='si')
+    parser.add_argument('--past', type=bool, default=None)
     parser.add_argument('--time', type=str, default='2019-11-15T00:00:00')
     args = parser.parse_args()
     
@@ -64,30 +56,29 @@ if __name__ == "__main__":
     LONGITUDE = args.long
     LATITUDE = args.lati
     UNITS = args.uni
+    NOW = args.past
 
-    start = datetime.strptime('2018-10-01', '%Y-%m-%d').date()
-    end   = datetime.strptime('2019-05-31', '%Y-%m-%d').date()
+    if NOW: 
+            
+        start = datetime.strptime('2018-10-01', '%Y-%m-%d').date()
+        end   = datetime.strptime('2018-10-10', '%Y-%m-%d').date()
 
-    def daterange(_start, _end):
-        for n in range((_end - _start).days):
-            yield _start + timedelta(n)
+        def daterange(_start, _end):
+            for n in range((_end - _start).days):
+                yield _start + timedelta(n)
 
+        #過去の一定期間を取り出す。
+        for times in daterange(start, end):
+            times = str(times)
+            time = times+'T00:00:00'
+            json_data = get_forecast_timemachine(apikey=args.api, long=args.long, lati=args.lati, uni=args.uni, time=time)
+            with open('./output/test/forecast-timemachine-'+times+'.json', 'w') as fp:
+                json.dump(json_data, fp, indent=4)
+        print("過去の天気予報を獲得しました。")
 
-    for times in daterange(start, end):
-        times = str(times)
-        time = times+'T00:00:00'
-        json_data = get_forecast_timemachine(apikey=args.api, long=args.long, lati=args.lati, uni=args.uni, time=time)
-        with open('./output/json/forecast-timemachine-'+times+'.json', 'w') as fp:
-            json.dump(json_data, fp, indent=4)
-
-
-
-    #EXCLUDE = currently,minutely,daily,alerts,flags
-
-    #json_data = get_forecast_today(apikey=args.api, long=args.long, lati=args.lati, uni=args.uni)
-    """json_data = get_forecast_timemachine(apikey=args.api, long=args.long, lati=args.lati, uni=args.uni, time=args.time)
+    else:
+        json_data = get_forecast_today(apikey=args.api, long=args.long, lati=args.lati, uni=args.uni)
     
-    with open('./output/forecast-timemachine-'+args.time+'-'+args.long+args.lati+'.json', 'w') as fp:
-        json.dump(json_data, fp, indent=4)"""
-    #change_data.to_csv("./output/"+args.long+args.lati+'.csv', encoding='utf-8')
-    #print('Complete!')
+        with open('./output/test/forecast.json', 'w') as fp:
+            json.dump(json_data, fp, indent=4)
+        print("今日の天気予報を獲得しました。")
